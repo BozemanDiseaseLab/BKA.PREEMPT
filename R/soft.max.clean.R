fa29a8bb70c49f320494333a8c377a30e79afc79
dncrwlye <dancrowley.e@gmail.com>
2019-04-15 11:17:26 -0600

    annotations

--- R/soft.max.clean.R
+++ R/soft.max.clean.R
@@ -11,27 +11,40 @@ soft.max.clean <- function(file_path, num_of_time_points)
 {
   library(tidyverse)
   library(readxl)
+  
+  #first, load in data using read_excel function, dont obtain col_names. this is reading in the raw softmax data
+  #it is a bizarre  format so we need to clean it up
   data <- readxl::read_excel(file_path, col_names = FALSE)
+  #the  softmax data often includes many rows of metadata about the run, however, the number of rows is for some
+  #reason really inconsisten, so we can't just use 'skip rows' argument in the  read_excel function
+  #instead, look for the word 'time', that is where the data starts. so let's find that row, and call it 'index' 
   index <- which(grepl(pattern = "Time", unlist(data[,1]))== TRUE)
+  #every row before  we first see the word "time', delete 
   data <- data[-c(0:index), ]
+  #only grabb the first 14 columns, everything else is GAAHHBAGE, to quote marky mark 
   data <- data[,1:14]
   
+  #okay now we need to read in the 'well's sheet. This how we know which sample  is in each well.
+  #without this data we are no better than the animals 
   wells <- readxl::read_excel(file_path, col_names = FALSE, sheet = 2)
   #load in experimental 'metadata'
   metadata <- readxl::read_excel(file_path, col_names = FALSE, sheet = 3)
   
+  #okay, labels all  the columns 
   colnames(data) <- c("time", "temp_c", "col_1", "col_2", "col_3","col_4","col_5","col_6","col_7","col_8","col_9","col_10","col_11","col_12")
 
+  #figure  out how  many time points are  in the data
   if(is.na(num_of_time_points))
   {
     num_of_time_points <- nrow(data[!is.na(data$time), ])
 
   }
-
+  #each timepoint is associated with 9 rows, so multiply 9* the number of time points to trim the end of the dataset
   end_of_data <- 9*num_of_time_points
+  #trim the blank rows at the end of the data
   data <- data[c(0:end_of_data),]
 
-  #remove blanks
+  #remove blanks, which exist between every time point in the softmax data. stupid goddamn softmax
   blanks <- seq(9,end_of_data,by=9)
   data <- data[-c(blanks),]
 
@@ -39,6 +52,7 @@ soft.max.clean <- function(file_path, num_of_time_points)
   times <-  seq(1,end_of_data-9,by=8)
   index <- 0
 
+  #i  honestly do not remember what im doing here
   data$time_2 <- NA
   for(i in (times))
   {
@@ -50,22 +64,28 @@ soft.max.clean <- function(file_path, num_of_time_points)
   data$time_2 <- NULL
   data$which_row <- LETTERS[1:8]
 
+  #tidy up the data, putting it into a 'tidy' format. this is the format  we  should use for  the anlayses 
   data.tidy <- data %>%
     select(-c(temp_c)) %>%
     group_by(time, which_row) %>%
     gather(key=column, value = measure, 2:13)
 
+  #label the wells in the 'well' datasheet, so we can join it with the actual raw softmax data
   colnames(wells) <- c("col_1", "col_2", "col_3","col_4","col_5","col_6","col_7","col_8","col_9","col_10","col_11","col_12")
   wells$which_row <- LETTERS[1:8]
-
+  
   wells.tidy <- wells %>%
     group_by(which_row) %>%
     gather(key=column, value = sample, 1:12)
 
+  #we have  now joined the raw data with the wells datasheet, which maps up  96 wells plate 
   data.tidy.join <- full_join(data.tidy, wells.tidy)
-  #add in experimental metadata
+  #add in experimental metadata, like who ran the assay, the bacteria,  etc. 
   data.tidy.join$bacteria = rep(as.character(metadata[3,3]),nrow(data.tidy.join)) 
   data.tidy.join$experiment_id <- rep(as.character(paste(metadata[1,3], metadata[2,3], sep = "_")), nrow(data.tidy.join))
-  
+  #"okay Caylee I hope you are happy with all this annotation" - Dan
+  #"oh wow this  sure  is great, Dan you are  da real  MPV" - Caylee
+  #" oh wow love this  gr8 communication, this project sure  is swell' -Dan
+  #"I am scretly Jared Kushner, please dont tell Raina' -Wyatt
   return(data.tidy.join)
 }
